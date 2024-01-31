0606a5d2d8f238bc9fd52e9567268e2915772666
jumormt <jumormt@163.com>
2024-01-31 11:35:36 +1100

    opaque bc

--- generate_bc.sh
+++ generate_bc.sh
@@ -94,12 +94,12 @@ for td in $test_dirs; do
     ########
     if test $td == "mem_leak"
     then
-        $compiler -Wno-everything -S -emit-llvm -fno-discard-value-names -g -I"$root" "$c_f" -o "$bc_f"
+        $compiler -Wno-everything -mllvm -opaque-pointers -S -emit-llvm -fno-discard-value-names -g -I"$root" "$c_f" -o "$bc_f"
     else
-        $compiler -Wno-everything -S -emit-llvm -fno-discard-value-names -I"$root" "$c_f" -o "$bc_f"
+        $compiler -Wno-everything -mllvm -opaque-pointers -S -emit-llvm -fno-discard-value-names -I"$root" "$c_f" -o "$bc_f"
     fi
     #llvm-as "$bc_f" -o "$bc_f"
-    opt -S -mem2reg "$bc_f" -o "$bc_f"
+    opt -opaque-pointers -S -mem2reg "$bc_f" -o "$bc_f"
   done
 done
 
